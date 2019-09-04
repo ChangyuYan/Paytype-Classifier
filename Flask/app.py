4ac3a4b07091ed2d38bedfe53fb00574eb29097b
ChangyuYan <changyannnnnn@gmail.com>
2019-09-04 23:15:28 +0800

    added string classifier

--- Flask/app.py
+++ Flask/app.py
@@ -24,16 +24,17 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     # Read from user input
-    int_features = np.array([float(x) for x in request.form.values()])
-    int_features = int_features.reshape((1, -1))
+    float_features = np.array([float(x) for x in request.form.values()])
+    float_features = float_features.reshape((1, -1))
 
     # Prediction
-    pred_use_hours = rf_use_hours.predict(int_features)[0]
-    pred_use_salaries = rf_use_salaries.predict(int_features)[0]
-    pred_is_productive = rf_is_productive.predict(int_features)[0]
+    pred_use_hours = rf_use_hours.predict(float_features)[0]
+    pred_use_salaries = rf_use_salaries.predict(float_features)[0]
+    pred_is_productive = rf_is_productive.predict(float_features)[0]
 
 
-    return render_template('results.html', pred_use_hours = pred_use_hours, pred_use_salaries = pred_use_salaries, pred_is_productive = pred_is_productive)
+    return render_template('results.html', pred_use_hours = pred_use_hours, pred_use_salaries = pred_use_salaries, pred_is_productive = pred_is_productive,
+    total_dollars = float_features[0][0], total_hours = float_features[0][1], hourly_rate = float_features[0][2])
 
 
 
