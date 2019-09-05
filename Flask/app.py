977e920a6a5aa2025367f82b686c6dd94da20eb5
ChangyuYan <changyannnnnn@gmail.com>
2019-09-05 13:24:39 +0800

    added string classifier to the web app

--- Flask/app.py
+++ Flask/app.py
@@ -12,6 +12,9 @@ rf_use_hours = pickle.load(open("models/RandomForest_use_hours.pkl", "rb"))
 rf_use_salaries = pickle.load(open("models/RandomForest_use_salaries.pkl", "rb"))
 rf_is_productive = pickle.load(open("models/RandomForest_is_productive.pkl", "rb"))
 
+# Loading the String Classifier
+string_classifier = pickle.load(open("models/string_classifier.pkl", "rb"))
+
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -24,13 +27,22 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     # Read from user input
-    float_features = np.array([float(x) for x in request.form.values()])
+    input_features = [x for x in request.form.values()]
+    payment_type_string = str.upper(str(input_features[0]))
+    float_features = input_features[1:]
+    float_features = np.array([float(x) for x in float_features])
     float_features = float_features.reshape((1, -1))
 
     # Prediction
-    pred_use_hours = rf_use_hours.predict(float_features)[0]
-    pred_use_salaries = rf_use_salaries.predict(float_features)[0]
-    pred_is_productive = rf_is_productive.predict(float_features)[0]
+    if payment_type_string in string_classifier:
+        pred_use_hours = string_classifier[payment_type_string]['use_hours']
+        pred_use_salaries = string_classifier[payment_type_string]['use_salaries']
+        pred_is_productive = string_classifier[payment_type_string]['is_productive']
+
+    else:
+        pred_use_hours = rf_use_hours.predict(float_features)[0]
+        pred_use_salaries = rf_use_salaries.predict(float_features)[0]
+        pred_is_productive = rf_is_productive.predict(float_features)[0]
 
 
     return render_template('results.html', pred_use_hours = pred_use_hours, pred_use_salaries = pred_use_salaries, pred_is_productive = pred_is_productive,
